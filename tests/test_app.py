385f1586364d9807f104c11a4a385c1277b202f3
Jessica Kelly <JessKelly91@outlook.com>
2023-12-01 10:12:49 -0500

    updating tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -3,50 +3,47 @@ from unittest import TestCase
 from app import app
 from models import User, db
 
-"""
-TO TEST:
-- status code of each route
-- be sure to follow redirects
-"""
-
-#use test database
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly_test'
-app.config['SQLALCHEMY_ECHO'] = False
-
-#make flask errors be real errors - rather than HTML pages with error info
-app.config['TESTING'] = True
-
-#don't use flask DebugToolbar
-app.config['DEBUG_TB_HOSTS'] = ['dont-show-debug-toolbar']
-
-
-db.drop_all()
-db.create_all()
 
 class User_Views(TestCase):
 
     def setUp(self):
         """Clean up any existing users."""
-        User.query.delete()
+        with app.app_context():
+            #use test database
+            app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly_test'
+            app.config['SQLALCHEMY_ECHO'] = False
 
-        user = User(first_name="Jessica", last_name="Kelly", image_url='https://tinyurl.com/mrx6kek8')
+            #make flask errors be real errors - rather than HTML pages with error info
+            app.config['TESTING'] = True
 
-        self.user_id = user.id
+            #don't use flask DebugToolbar
+            app.config['DEBUG_TB_HOSTS'] = ['dont-show-debug-toolbar']
+
+            db.drop_all()
+            db.create_all()
+
+            User.query.delete()
+
+            user = User(first_name="Jessica", last_name="Kelly", image_url='https://tinyurl.com/mrx6kek8')
+            db.session.add(user)
+            db.session.commit()
+
+            self.user_id = user.id
     
 
-    def tearDown(self):
-        """clean up any fouled transaction"""
-        db.session.rollback()
-
+    # def tearDown(self):
+    #     """clean up any fouled transaction"""
+    #     db.session.rollback()
+    
     def test_list_users(self):
-        """Test the homepage"""
-        with app.test_client() as client:
-            resp = client.get('/users')
-            html = resp.get_data(as_text = True)
+            """Test the homepage"""
+            with app.test_client() as client:
+                resp = client.get('/users')
+                html = resp.get_data(as_text = True)
 
-            self.assertEqual(resp.status_code, 200)
-            self.assertIn('<h2>Users:</h2>', html)
-            self.assertIn('Jessica Kelly', html)
+                self.assertEqual(resp.status_code, 200)
+                self.assertIn('<h2>Users:</h2>', html)
+                self.assertIn('Jessica', html)
 
 
     def test_show_new_user_form(self):
@@ -76,14 +73,15 @@ class User_Views(TestCase):
             html = resp.get_data(as_text = True)
 
             self.assertEqual(resp.status_code, 200)
-            self.assertIn('<button id="cancel-edit-button">', html)
+            self.assertIn('<a class="btn btn-info" id="cancel-edit-user" href="/users/1" role="button">Cancel</a>', html)
 
     def test_delete_user_redirect(self):
         """Test delete route
-           Follow redirect and be sure name is not in html response
+        Follow redirect and be sure name is not in html response
         """
         with app.test_client() as client:
-            resp = client.get(f'/users/{self.user_id}/delete', follows_redirects=True)
+            resp = client.get(f'/users/{self.user_id}/delete', follow_redirects=True)
             html = resp.get_data(as_text = True)
             
-            self.assertEqual(resp.status_code, 302)
+            self.assertEqual(resp.status_code, 200)
+            self.assertNotIn('Jessica',html)
